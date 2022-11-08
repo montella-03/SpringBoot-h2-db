a0b2a8a7222f79a0c85e4924d9c20536d8101182
montella-03 <oigoc60@gmail.com>
2022-11-08 15:37:47 +0300

    commit 2

---
+++ src/test/java/com/montella/sample/repository/DepartmentRepoTest.java
@@ -0,0 +1,22 @@
+//package com.montella.sample.repository;
+//
+//import com.montella.sample.entity.Department;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class DepartmentRepoTest {
+//    private DepartmentRepo repo;
+//    @Test
+//    public void saveDepartment(){
+//        Department department = Department.builder()
+//                .departmentAddress("234d")
+//                .departmentName("AI")
+//                .departmentCode("23R-56")
+//                .build();
+//        repo.save(department);
+//    }
+//
+//}
